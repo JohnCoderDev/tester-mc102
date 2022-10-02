65b7196b4cbbe0e19e0a92d9668068cf388bf308
JohnCoderDev <jfuregatti@gmail.com>
2022-10-02 14:20:30 -0300

    modifying Tester to class instead functions

--- tester.py
+++ tester.py
@@ -1,153 +1,182 @@
-
-# author: johncoderdev
-# creation date: 01/10/2022
-# license: MIT
-# github repository:
-
-# imports
+"""
+author: johncoderdev
+creation date: 02/10/2022
+license: MIT
+github repository:
+"""
 from sys import argv
 from subprocess import Popen, PIPE
 from glob import glob
-from os.path import basename, exists
+from os.path import basename, exists, isdir
 
-from pynvim import encoding
-
-# função para verificar se a resposta bate com 
-# o resultado esperado
-def verifyAnswer(programName, inputPath, outputPath) -> tuple:    
-    """
-    Está é uma função para testar o programa passado como
-    argumento. O seu retorno normal será uma tupla que 
-    conterá a saída esperada, a saída obtida, e as respostas
-    que não bateram. Caso contrário, retorna o erro obtido
-    na execução do programa.
-    (param) programName: nome do arquivo '.py' que será testado.
-    (param) inputPath: caminho até o arquivo que contém as entradas do programa.
-    (param) outputPath: caminho até o arquivo que contém as saídas esperadas para o programa.
-    """
-    command = f'python {programName}'
-    # Roda a execução do programa
-    process = Popen(command, 
-                    stdout=PIPE,
-                    stdin=PIPE,
-                    stderr=PIPE,
-                    encoding='ISO-8859-1')
-        
-    with open(inputPath, 'r') as inputFile:
-        inputText = ''.join(inputFile.readlines())
-
-    # roda o programa passando as entradas
-    process.communicate(input=inputText)
-    # pega a saida do programa
-    programOutput, error = process.communicate()
-
-    # retorna o erro do progama caso exista
-    if error != '': return error
-
-    programOutput = programOutput.split('\n')
-
-    # caso não tenha dado erro, compara a saída
-    # do programa com a resposta esperada
-    with open(outputPath, 'r', encoding='utf-8') as outputFile:
-        expectedOutput = ''.join(outputFile.readlines())
-        expectedOutput = expectedOutput.split('\n')
-
-    incorrectAnswers = [
-        programOutput[a] for a in range(len(programOutput))
-        if programOutput[a] not in expectedOutput
-    ]
-    
-    return programOutput, expectedOutput, incorrectAnswers
-
-# Verifica se os caminhos passados existem
 def verifyPaths(*paths):
     """
-    Função para verificar se os caminhos existem. Caso
-    não exista o caminho especificado, retorna um erro.
-    (param) *paths: caminhos a serem avaliados.
+    Levanta uma exceção se algum caminho passado
+    não for encontrado.
     """
     for path in paths:
         if not exists(path):
             raise FileNotFoundError(
-                '\033[31mnão foi possível encontrar ' + 
-                f'\033[36m{path}\033[m'
+                f'\033[31marquivo \'{path}\' não foi encontrado.\033[m'
             )
 
-def verifyAllAnswers(programName: str, directoryWithAnswers: str):
+def verifyDir(*paths):
     """
-    Função para verificar as respostas do programa com base
-    nos arquivos presentes em um diretório com as respostas.
-    (param) programName: nome do programa a ser testado.
-    (param) directoryWithAnswers: caminho do diretório que contém as respostas.
+    Verifica se o caminho especificado é um diretório
+    e se existe. Levanta uma exceção caso não.
     """
-    filesInDirectory = glob(directoryWithAnswers + '/*.in')
-    filesPrefixes = [fn.replace('.in', '') for fn in filesInDirectory]
-    
-    for filePrefix in filesPrefixes:
+    for path in paths:
+        if not isdir(path):
+            raise FileNotFoundError(
+                f'\033[31mpasta \'{path}\' não foi encontrada.\033[m'
+            )
+
+class Tester:
+    """
+    Classe do Tester que será usado para
+    fazer os testes.
+    """
+    def __init__(self, programToBeTested: str, directoryWithAnswers: str) -> None:
+        if not programToBeTested.endswith('.py'): programToBeTested += '.py'
+        self.programToBeTested = programToBeTested
+        self.directoryWithAnswers = directoryWithAnswers
+
         try:
-            inputFile = glob(f'{filePrefix}.in')[0]
-            outputFile = glob(f'{filePrefix}.out')[0]
-        
-        except IndexError:
-            print(f'\033[31mnão foi possível encontrar o teste {filePrefix}\033[m')
-            continue
-
-        result = verifyAnswer(programName, inputFile, outputFile) 
-
-        if type(result) is str:
-            print(f'\033[41mocorreu um erro na execução do programa:\033[m')
-            print(f'\033[31m{result}\033[m')
-            continue
-
-        programOutput, expectedOutput, incorrectAnswer = result
-
-        # caso não haja respostas incorretas
-        if not incorrectAnswer:
-            print(f'\033[32mO teste {filePrefix} está correto\033[m')
-            continue
-        
-        print(f'\033[31m\033[1mO teste {filePrefix} está incorreto\033[m\n')
-
-        # printa as respostas esperadas
-        print('\033[1mResposta esperada:\033[m\n')
-        for line in expectedOutput:
+            verifyPaths(self.programToBeTested)
+            verifyDir(self.directoryWithAnswers)
             
-            if line not in programOutput:
-                print(f'\033[32m{line}\033[m')
-                continue
-            
-            print(line)
-        
-        # Printa as respostas do programa
-        print('\n\033[1mResposta obtida:\033[m\n')
-        for line in programOutput:
-            if line in incorrectAnswer:
-                print(f'\033[31m{line}\033[m')
-                continue
-
-            print(f'\033[32m{line}\033[m')
-        
-
-
-if __name__ == "__main__":
-    try:
-        # verifica se o comando foi digitado corretamente
-        if len(argv) != 3:
-            print('\033[33mPara usar este programa,' +
-                'você deve digitar o comando da ' + 
-                'seguinte forma:\n' +
-                f'\033[mpython {basename(__file__)} ' +
-                '\033[36m<nome do arquivo> <pasta com as respostas>\033[m')
+        except FileNotFoundError as error:
+            print(str(error))
             exit()
+    
+    def setProgramToBeTested(self, newProgram: str) -> None:
+        """
+        Muda o programa que será testado.
+        """
+        if not newProgram.endswith('.py'): newProgram += '.py'
+
+        try:
+            verifyPaths(newProgram)
+            self.programToBeTested = newProgram
+
+        except FileNotFoundError as error:
+            print(f'\033[31m{str(error.args)}\033[m')
+
+    def setDirectoryWithAnswers(self, directoryWithAnswers: str):
+        """
+        Permite mudar o diretório em que as respostas se encontram.
+        """
+        try:
+            verifyDir(directoryWithAnswers)
+            self.directoryWithAnswers = directoryWithAnswers
         
-        programToBeTested = argv[1] if argv[1].endswith('.py') else f'{argv[1]}.py'
-        directoryWithAnswers = argv[2]
+        except FileNotFoundError as error:
+            print(str(error))
+    
+    def verifyAnswer(self, inputFilePath: str, outputFilePath: str) -> tuple:
+        """
+        Verifica a resposta com base nos arquivos
+        de input e output passados.
+        """
+        command = f'python {self.programToBeTested}'
 
-        verifyPaths(programToBeTested, directoryWithAnswers)
-        verifyAllAnswers(programToBeTested, directoryWithAnswers)
+        process = Popen(
+            command,
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=PIPE,
+            encoding='ISO-8859-1'
+        )
 
-    except FileNotFoundError as e:
-        print(e.__str__())
+        try:
+            if not inputFilePath.endswith('.in'): inputFilePath += '.in'
+            if not outputFilePath.endswith('.out'): outputFilePath += '.out'
 
-    except ValueError as e:
-        print(e.__str__())
+            with open(inputFilePath, 'r') as inputFile:
+                inputText = ''.join(inputFile.readlines())
+            
+            with open(outputFilePath, 'r', encoding='utf-8') as outputFile:
+                expectedOutput = ''.join(outputFile.readlines())
+                expectedOutput = expectedOutput.split('\n')
+
+            process.communicate(inputText)
+            output, error = process.communicate()
+            output = output.split('\n')
+
+            if error != '':
+                raise RuntimeError(
+                    f'\033[41mocorreu um problema na execução do programa:\033[0m\n' + 
+                    f'\033[31m{error}\033[m'
+                )
+            
+            incorrectAnswers = [
+                output[answer] for answer in range(len(output))
+                if not output[answer] in expectedOutput
+            ]
+
+            return output, expectedOutput, incorrectAnswers
+
+        except FileNotFoundError as error:
+            print(f'\033[31mnão foi possível encontrar {error.filename}.\033[m')
+            return None
+
+        except RuntimeError as error:
+            print(str(error))
+            return None
+
+    def verifyAllAnswers(self) -> None:
+        """
+        Função para verificar as respostas no diretório especificado.
+        """
+        inputFilesInDirectory = [
+            filename.replace('.in', '') for filename in 
+            glob(self.directoryWithAnswers + '/*.in')
+        ]
+
+        for filename in inputFilesInDirectory:
+            result = self.verifyAnswer(filename, filename)
+
+            if result is None: continue
+
+            output, expectedOutput, incorrectAnswers = result
+
+            if not incorrectAnswers:
+                print(f'\033[32mO teste {filename} está correto.\033[m')
+                continue
+
+            print(f'\033[31mO teste {filename} está incorreto.\033[m\n')
+            print('\033[1mResultado esperado:\033[m\n')
+            
+            # printa as respostas esperadas
+            for expectedAnswer in expectedOutput:
+                
+                if not expectedAnswer in output:
+                    print(f'\033[32m{expectedAnswer}\033[m')
+                    continue
+                
+                print(expectedAnswer)
+            
+            print('\033[1m\nResultado obtido:\033[m\n')
+
+            # printa as respostas obtidas do programa
+            for answer in output:
+                
+                if answer in incorrectAnswers:
+                    print(f'\033[31m{answer}\033[m')
+                    continue
+
+                print(f'\033[32m{answer}\033[m')
+
+if __name__ == '__main__':
+    
+    if len(argv) != 3:
+        print(
+            '\033[31mpara usar este programa, você deve ' + 
+            'digitar o comando da seguinte forma:\033[m\n' + 
+            f'python {basename(__file__)} \033[33m ' + 
+            '<nome do arquivo> <pasta com as respostas>\033[m'
+        )
+        exit()
+
+    tester = Tester(argv[1], argv[2])
+    tester.verifyAllAnswers()
